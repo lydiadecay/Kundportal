522290ae3a9c9d7e282d6303516ef3c65d852a17
96simben <simoncraft3@hotmail.com>
2020-06-08 11:20:22 +0200

    LOGIN

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-import Login from "./login";
 import Index from "./Components/indexComponent";
 import "./Styling/App.css";
+import users from "./Data/users.json";
 
 class App extends React.Component {
   render() {
-    return <Login />;
+    return(
+    //<Login />
+    <div>
+    <form>
+      <label>First name:</label>
+        <input type="text" id="username" name="fname"/><br/>
+        <label>Last name:</label>
+        <input type="text" id="password" name="fname"/><br/>
+    </form>
+
+    <button onClick={check}> login </button>
+    </div>
+    )}
+}
+
+const check = () => {
+  const usernameC = document.getElementById("username").value;
+  const passwordC = document.getElementById("password").value;
+
+  if (usernameC === users[0].username && passwordC === users[0].password){
+    console.log("succes");
   }
 }
 
