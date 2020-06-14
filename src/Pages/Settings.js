851109d1a9782620aae192ed628f5d9d43281a80
96simben <simoncraft3@hotmail.com>
2020-06-14 15:28:32 +0200

    routes

---
+++ src/Pages/Settings.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Settings = (props) => {
+    console.log("settings page do something")
+    return <div>This is my Settings page</div>;
+
+}
+
+export default Settings
---
+++ src/Pages/StartPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+const StartPage = (props) => {
+    console.log(props)
+    return <div>you are on the start page</div>;
+
+}
+
+export default StartPage
